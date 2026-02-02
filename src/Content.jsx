7437d937383a48fd912bb13e44b237af19a874ed
theboogeyman81 <theboogeyman8144@gmail.com>
2026-02-02 22:46:58 +0530

    code

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,18 +1,24 @@
+import { useState } from 'react'
+
+
 const Content = () => {
+  const [name, setName] = useState('World')
+  const names = ['John', 'Jane', 'Jim', 'Jill']
 
-    const names = ['John', 'Jane', 'Jim', 'Jill']
-    const int = Math.floor(Math.random() * names.length)
-    const name = names[int]
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * names.length)
+    setName(names[randomIndex])
+  }
 
-
-    const handleClick = () => {
-        console.log('You clicked it!')
-    }
+  const handleClick2 = (clickedName) => {
+    console.log(`${clickedName} was clicked`)
+  }
 
   return (
     <main>
         <p>Hello {name}</p>
         <button onClick={handleClick}>Click it</button>
+        <button onClick={() => handleClick2('Dave')}>Clicked</button>
     </main>
   )
 }
